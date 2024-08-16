9acdb317a89dd7f11d45b94db27fd3a1b14827e1
Vignesh N <vigneshrage@gmail.com>
2024-08-16 14:43:19 +0530

    added layout for playbar and retrieved song list through js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Let's write some Javascript!");
+let getSongs = async () =>{
+    let a = await fetch("http://127.0.0.1:5500/assets/Songs/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML= response;
+    let as = div.getElementsByTagName("a")
+    
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.title.endsWith(".mp3")){
+            songs.push(element.title)
+        }
+    }
+    return songs
+}
+
+
+
+
+
+
+ main = async () =>{
+    let songs = await getSongs()
+    console.log(songs);
+
+}
+
+main()
+
+
+
